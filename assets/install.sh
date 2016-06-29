f1c6b5a25b899110bfc2574e1cb6f4863c56602e
root <root@vps285621.ovh.net>
2016-06-29 10:28:04 +0200

    enable postfix only from docker local network

--- assets/install.sh
+++ assets/install.sh
@@ -30,26 +30,11 @@ postconf -e myhostname=$maildomain
 postconf -F '*/*/chroot = n'
 
 ############
-# SASL SUPPORT FOR CLIENTS
-# The following options set parameters needed by Postfix to enable
-# Cyrus-SASL support for authentication of mail clients.
+# Authorize only requests from docker local network (172.17.0.0/16)
 ############
 # /etc/postfix/main.cf
-postconf -e smtpd_sasl_auth_enable=yes
-postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# smtpd.conf
-cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-pwcheck_method: auxprop
-auxprop_plugin: sasldb
-mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
-EOF
-# sasldb2
-echo $smtp_user | tr , \\n > /tmp/passwd
-while IFS=':' read -r _user _pwd; do
-  echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
-done < /tmp/passwd
-chown postfix.sasl /etc/sasldb2
+postconf -e smtpd_recipient_restrictions=permit_mynetworks,reject_unauth_destination
+postconf -e mynetworks=172.17.0.0/16
 
 ############
 # Enable TLS
